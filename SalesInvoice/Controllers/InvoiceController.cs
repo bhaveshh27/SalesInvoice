646eb594916196b4c02c657e675f94f98312f64e
Bhavesh Shrimali <shrimalibhavesh4@gmail.com>
2024-07-05 21:16:13 +0530

    nuget added model added

---
+++ SalesInvoice/Controllers/InvoiceController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SalesInvoice.Models;
+
+namespace SalesInvoice.Controllers
+{
+    public class InvoiceController : Controller
+    {
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ SalesInvoice/Models/Customer.cs
@@ -0,0 +1,11 @@
+﻿namespace SalesInvoice.Models
+{
+    public class Customer
+    {
+        public int CustomerId { get; set; }
+        public string Company { get; set; }
+        public string Address { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+    }
+}
